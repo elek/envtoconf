5ca141c68616cca671e6caca89dcb29fd4b38627
Márton Elek <elek@apache.org>
2019-12-13 22:30:50 +0100

    enable to define the format of a specific file

--- app/converter.go
+++ app/converter.go
@@ -12,9 +12,9 @@ type Configfile struct {
 }
 
 // Parse a environment variable key according to the name convention.
-func ParseKey(declaration string) (filename string, format string, key string, err error) {
+func ParseKey(declaration string, envs map[string]string) (filename string, format string, key string, err error) {
 	parts := strings.FieldsFunc(declaration, func(r rune) bool {
-		return (r == '.' || r == '_')
+		return r == '.' || r == '_'
 	})
 	if len(parts) == 0 {
 		return "", "", "", errors.New("Line can't be parsed: " + declaration)
@@ -24,8 +24,8 @@ func ParseKey(declaration string) (filename string, format string, key string, e
 	config_key := ""
 	if len(parts) > 1 {
 		extension = strings.ToLower(parts[1])
-		if len(declaration) > len(name) + len(extension) + 2 {
-			config_key = strings.TrimSpace(declaration[len(name) + len(extension) + 2:])
+		if len(declaration) > len(name)+len(extension)+2 {
+			config_key = strings.TrimSpace(declaration[len(name)+len(extension)+2:])
 		}
 
 		if strings.Contains(extension, "!") {
@@ -34,7 +34,9 @@ func ParseKey(declaration string) (filename string, format string, key string, e
 			})
 			extension = splitted[0]
 			format = splitted[1]
-			config_key = strings.TrimSpace(declaration[len(name) + len(extension) + len(format) + 3:])
+			config_key = strings.TrimSpace(declaration[len(name)+len(extension)+len(format)+3:])
+		} else if definedFormat, ok := envs[strings.ToUpper(name)+"."+strings.ToUpper(extension)]; ok {
+			format = strings.ToLower(definedFormat)
 		} else {
 			format = extension
 		}
@@ -62,8 +64,8 @@ func TransformToString(content map[string]string, format string) (string, error)
 func ParseKeyValues(envs map[string]string) map[string]Configfile {
 	var result = make(map[string]Configfile)
 	for key, value := range envs {
-		filename, format, key, err := ParseKey(key)
-		if err == nil {
+		filename, format, key, err := ParseKey(key, envs)
+		if err == nil && len(key) > 0 {
 			if configfile, ok := result[filename]; ok {
 				configfile.Entries[key] = value
 			} else {
